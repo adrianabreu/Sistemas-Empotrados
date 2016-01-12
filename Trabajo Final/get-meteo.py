a594c5a8882bdc40632d43e0fa5ffbba987ad758
Andres Cidoncha <andrescidonchacarballo@gmail.com>
2016-01-12 16:33:52 +0000

    Little changes

--- Trabajo Final/get-meteo.py
+++ Trabajo Final/get-meteo.py
@@ -3,12 +3,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-#http://www.eltiempo.tv/Santa-Cruz-de-Tenerife/Santa-Cruz-de-Tenerife.html
-url = "http://www.eltiempo.tv/"
-ciudad="Santa Cruz de Tenerife"
-
-def get_url(ciudad):
-	aux=ciudad.replace(' ','-')
+def get_url(city_name):
+    #EJEMPLO: http://www.eltiempo.tv/Santa-Cruz-de-Tenerife/Santa-Cruz-de-Tenerife.html
+    url = "http://www.eltiempo.tv/"
+	aux=city_name.replace(' ','-')
 	aux=url + aux + '/' + aux + ".html"
 	return aux
 
@@ -24,17 +22,23 @@ def get(html, search_att, exp1, exp2):
     
 	return att
 
-# Realizamos la petición a la web
-url=get_url(ciudad)
-req = requests.get(url)
+def get_meteo(city_name):
+	# Realizamos la petición a la web
+    url = get_url(city_name)
+    req = requests.get(url)
+    meteo = dict()
 
-# Comprobamos que la petición nos devuelve un Status Code = 200
-statusCode = req.status_code
-if statusCode == 200:
-    html = BeautifulSoup(req.text, "lxml")
-    horas = get(html,"detallada_hora",'>','<')
-    temps = get(html,"detallada_icono",'title=\"','\">')
+	# Comprobamos que la petición nos devuelve un Status Code = 200
+    statusCode = req.status_code
+    if statusCode == 200:
+        html = BeautifulSoup(req.text, "lxml")
+        hours = get(html,"detallada_hora",'>','<')
+        temps = get(html,"detallada_icono",'title=\"','\">')
 
-    for i in range(len(horas)):
-    	print("Hora: %s Prevision: %s" % (horas[i],temps[i]))
+        for i in range(len(horas)):
+            meteo[horas[i]]=temps[i]
 
+    return meteo
+
+if __name__ == '__main__':
+    print get_meteo("Santa Cruz de Tenerife")
